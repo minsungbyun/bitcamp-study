a0cb674022cf77b756e92ec8bb507cf94323af72
minsungbyun <enjoy8939@naver.com>
2021-07-12 22:17:59 +0900

    21/0712

---
+++ java-basic/src/main/java/com/eomcs/lang/ex01/Exam111111.java
@@ -0,0 +1,9 @@
+package ex01;
+
+public class Exam111111 {
+
+  public static void main(String[] args) {
+    System.out.println("ff");
+  }
+
+}
--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -1,73 +1,70 @@
 package com.eomcs.pms;
 
-import java.util.Date;
+import java.sql.Date;
 import java.util.Scanner;
 
-// 1) 배열 사용 전
-// 2) 배열 사용 후
-// 3) 반복문 적용 : while문
-// 4) 반복문 적용 : for문
-// 5) 여러 문장에서 반복적으로
-// 6) 조회용으로만 사용할 변수라면 상수로 선언한다.
-// 7) 특정 조건에 따라 반복을 멈춘다.
-// 8) 날짜의 출력형식을 "yyyy-MM-dd"로 변경한다.
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("[회원]");
-    // 7단계 : 인원수에 맞게 출력하고 싶다.
-
-    final int MAX_SIZE = 3; // 중복되는 수를 상수로 뺀다.
-    int size = 0; // 기본 0명
-
-    int[] no = new int[MAX_SIZE];
-    String[] name = new String[MAX_SIZE];
-    String[] email = new String[MAX_SIZE];
-    String[] password = new String[MAX_SIZE];
-    String[] photo = new String[MAX_SIZE];
-    String[] tel = new String[MAX_SIZE];
-    Date[] registerdDate = new Date[MAX_SIZE];
 
     Scanner keyboardScan = new Scanner(System.in);
 
-    for (int i = 0; i < MAX_SIZE; i = i + 1) {
-      System.out.print("번호? ");
-      no[i] = Integer.parseInt(keyboardScan.nextLine());
-      System.out.print("이름? ");
-      name[i] = keyboardScan.nextLine();
-      System.out.print("이메일? ");
-      email[i] = keyboardScan.nextLine();
-      System.out.print("암호? ");
-      password[i] = keyboardScan.nextLine();
-      System.out.print("사진? ");
-      photo[i] = keyboardScan.nextLine();
-      System.out.print("전화? ");
-      tel[i] = keyboardScan.nextLine();
-      registerdDate[i] = new Date();
-      size = size + 1; // 1명이 입력 완료하였다.
-      System.out.println();
+    // 최대 입력 개수
+    final int LENGTH = 100;
 
-      System.out.println("계속 입력하시겠습니까? (y/N)");
+    int[] no = new int[LENGTH];
+    String[] name = new String[LENGTH];
+    String[] email = new String[LENGTH];
+    String[] password = new String[LENGTH];
+    String[] photo = new String[LENGTH];
+    String[] tel = new String[LENGTH];
+    Date[] registeredDate = new Date[LENGTH];
+
+    int size = 0;
+
+    while (true) {
+      System.out.print("명령> ");
       String input = keyboardScan.nextLine();
-      if (input.equals("N") || input.equals("")) {
+
+      if (input.equalsIgnoreCase("exit") || input.equalsIgnoreCase("quit")) {
+        System.out.println("안녕!");
         break;
+      } else if (input.equals("/member/add")) {
+        System.out.println("[회원등록]");
+        System.out.print("번호? ");
+        no[size] = Integer.parseInt(keyboardScan.nextLine());
+
+        System.out.print("이름? ");
+        name[size] = keyboardScan.nextLine();
+
+        System.out.print("이메일? ");
+        email[size] = keyboardScan.nextLine();
+
+        System.out.print("암호? ");
+        password[size] = keyboardScan.nextLine();
+
+        System.out.print("사진? ");
+        photo[size] = keyboardScan.nextLine();
+
+        System.out.print("전화? ");
+        tel[size] = keyboardScan.nextLine();
+
+        registeredDate[size] = new java.sql.Date(System.currentTimeMillis());
+
+        size++;
+
+      } else if (input.equals("/member/list")) {
+        System.out.println("[회원목록]");
+        for (int i = 0; i < size; i++) {
+          // 번호, 이름, 이메일, 전화, 가입일
+          System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
+              no[i], name[i], email[i], tel[i], registeredDate[i]);
+        }
+      } else {
+        System.out.println("실행할 수 없는 명령어입니다.");
       }
+      System.out.println();
     }
-
-    keyboardScan.close(); // 데이터 입출력이 끝났으면 도구를 닫는다.
-
-    System.out.println("--------------------------------");
-    // 인원수에 맞게 출력하도록 설정한다.
-    for (int i = 0; i < size; i = i + 1) {
-      System.out.printf("%d, %s, %s, %s, %s, %s, %tY-%7$tm-%7$td\n",
-          no[i],
-          name[i],
-          email[i],
-          password[i],
-          photo[i],
-          tel[i],
-          registerdDate[i]);
-    }
-
+    keyboardScan.close();
   }
 }
